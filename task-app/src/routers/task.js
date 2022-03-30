aa90459a0a52f7460f6725d9cdf9ebdaaecbf8f7
pedrofhasler <pedro.hasler@gmail.com>
2022-03-30 13:52:06 -0300

    adding router and refatoring routes

---
+++ task-app/src/routers/task.js
@@ -0,0 +1,82 @@
+import express from "express";
+import { Task } from "../models/task.js";
+
+const router = new express.Router()
+
+export const taskRouter = (app) => {
+
+    router.post('', async(req, res) => {
+        const task = new Task(req.body)
+
+        try {
+            await task.save()
+            res.status(201).send(task)
+        } catch (error) {
+            res.status(400).send(e)
+        }
+    })
+
+    router.get('', async(req, res) => {
+
+        try {
+            const tasks = await Task.find({})
+            res.send(tasks)
+        } catch (error) {
+            res.status(500).send()
+        }
+    })
+
+    router.get('/:id', async(req, res) => {
+        const _id = req.params.id
+
+        try {
+            const task = await Task.findById(_id)
+
+            if (!task) {
+                return res.status(404).send
+            }
+
+            res.send(task)
+        } catch (error) {
+            res.status(500)
+        }
+    })
+
+    router.patch('/:id', async(req, res) => {
+        const updates = Objects.keys(req.body)
+        const allowedUpdates = ['description', 'completed']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+        if (!isValidOperation) {
+            return res.status(400).send({ error: 'Invalid updates!' })
+        }
+
+        try {
+            const task = await Task.findByIdAndUpdate(res.params.id, req.body, { new: true, runValidators: true })
+
+            if (!task) {
+                return res.status(404).send()
+            }
+
+        } catch (error) {
+            res.status(400).send()
+        }
+    })
+
+    router.delete('/:id', async(req, res) => {
+        try {
+            const task = await Task.findByIdAndDelete(req.params.id)
+
+            if (!task) {
+                return res.status(404).send()
+            }
+
+            res.send(task)
+
+        } catch (error) {
+            res.status(500).send()
+        }
+    })
+
+    app.use("/tasks", router)
+}
---
+++ task-app/src/routers/user.js
@@ -0,0 +1,85 @@
+import express from "express";
+import { User } from "../models/user.js";
+
+const router = new express.Router()
+
+export const userRouter = (app) => {
+
+    router.post('', async(req, res) => {
+        const user = new User(req.body)
+
+        try {
+            await user.save()
+            res.status(201).send(user)
+        } catch (e) {
+            res.status(400).send(e)
+
+        }
+    })
+
+    router.get('', async(req, res) => {
+
+        try {
+            const users = await User.find({})
+            res.send(users)
+        } catch (error) {
+            res.status(500).send()
+        }
+    })
+
+    router.get('/:id', async(req, res) => {
+        const _id = req.params.id
+
+        try {
+            const user = await User.findById(_id)
+
+            if (!user) {
+                return res.status(404).send
+            }
+
+            res.send(user)
+        } catch (error) {
+            res.status(500)
+        }
+    })
+
+    router.patch('/:id', async(req, res) => {
+        const updates = Objects.keys(req.body)
+        const allowedUpdates = ['name', 'email', 'password', 'age']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+        if (!isValidOperation) {
+            return res.status(400).send({ error: 'Invalid updates!' })
+        }
+
+        try {
+            const user = await User.findByIdAndUpdate(res.params.id, req.body, { new: true, runValidators: true })
+
+            if (!user) {
+                return res.status(404).send()
+            }
+
+            res.status(200).send(user)
+
+        } catch (error) {
+            res.status(400).send()
+        }
+    })
+
+    router.delete('/:id', async(req, res) => {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id)
+
+            if (!user) {
+                return res.status(404).send()
+            }
+
+            res.send(user)
+
+        } catch (error) {
+            res.status(500).send()
+        }
+    })
+
+    app.use("/users", router)
+}
